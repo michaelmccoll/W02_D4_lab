39ea85b2e33cbe7d7e135ec7b7939e80f2ee1445
michaelmccoll <mccollmichael@hotmail.com>
2021-03-04 17:03:24 +0000

    High scores completed

--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -5,9 +5,6 @@ import unittest
 from src.high_scores import *
 
 class HighScoresTest(unittest.TestCase):
-
-    # Tests
-
     # Test latest score (the last thing in the list)
     def test_latest_scores(self):
         scores = [23,43,565,34,656,76,56,5]
@@ -36,6 +33,11 @@ class HighScoresTest(unittest.TestCase):
         self.assertEqual([20,20,20],personal_top_three(scores))
 
     # Test top three when there are less than three
+    def test_top_three_when_less_then_3(self):
+        scores = [20,10,20,15,16]
+        self.assertEqual([20,20,16],personal_top_three(scores))
 
     # Test top three when there is only one
-
+    def test_top_three_when_only_1(self):
+        scores = [20,10,12,15,16]
+        self.assertEqual([20,16,15],personal_top_three(scores))
