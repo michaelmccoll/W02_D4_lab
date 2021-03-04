361dfd0317be56cfd4b0e8e297c4660050dc618f
michaelmccoll <mccollmichael@hotmail.com>
2021-03-04 15:31:25 +0000

    High scores progress

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -1,10 +1,21 @@
-def latest(scores):
-    pass
-
+def latest_score(scores):
+    last_score = scores[-1]
+    return last_score
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
+#changing and reassinging the actual scores list = could be bad if you want to use the scores later on i.e get the latest score 
+# def personal_top_three(scores):
+#     scores.sort(reverse=True)
+#     return scores[0:3]
 
+# creating a new list of the sorted list = the original data NOT mutated
 def personal_top_three(scores):
-    pass
+    sorted_scores = sorted(scores)
+    return sorted_scores[-3:][::-1]
+
+def highest_to_lowest(scores):
+    scores.sort(reverse=True)
+    return scores
+
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -1,25 +1,41 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+# from src.high_scores import latest, personal_best, personal_top_three
 
-# Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
-
+from src.high_scores import *
 
 class HighScoresTest(unittest.TestCase):
-    
+
     # Tests
 
     # Test latest score (the last thing in the list)
+    def test_latest_scores(self):
+        scores = [23,43,565,34,656,76,56,5]
+        self.assertEqual(5, latest_score(scores))
 
     # Test personal best (the highest score in the list)
+    def test_personal_best(self):
+        scores = [23,43,565,34,656,76,56,5]
+        self.assertEqual(656, personal_best(scores))
 
     # Test top three from list of scores
+    def test_top_three(self):
+        scores = [23,43,565,34,656,76,56,5]
+        top_three = [656,565,76]
+        self.assertEqual(top_three, personal_top_three(scores))
 
-    # Test ordered from highest tp lowest
-
+    # Test ordered from highest to lowest
+    def test_highest_to_lowest(self):
+        scores = [23,43,565,34,656,76,56,5]
+        scores_reversed = [656,565,76,56,43,34,23,5]
+        self.assertEqual(scores_reversed, highest_to_lowest(scores))
+        
     # Test top three when there is a tie
+    def test_top_three_tie(self):
+        scores = [20,10,20,15,16,20]
+        self.assertEqual([20,20,20],personal_top_three(scores))
 
     # Test top three when there are less than three
 
     # Test top three when there is only one
-    
+
